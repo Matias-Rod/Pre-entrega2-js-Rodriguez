2c28562cd090d2e548a7d3926d6b8cfa0c978a1b
Matias Rodriguez <matiezequielr@gmail.com>
2023-08-15 17:17:21 -0300

    incio de repo, se agrego index.html  y main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+const alumno = prompt("Ingrese el nombre del alumno") ;
+const notaPrimerTrimestre = parseFloat(prompt("Ingrese nota del primer trimestre")) ;
+const notaSegundoTrimestre = parseFloat(prompt("Ingrese nota del segundo trimestre")) ;
+const notaTercerTrimestre = parseFloat(prompt("Ingrese nota del tercer trimestre")) ;
+
+
+function calcularPromedio(nota1, nota2, nota3) {
+    return (nota1 + nota2 + nota3) / 3;
+}
+
+const notaPromedio =calcularPromedio(notaPrimerTrimestre, notaSegundoTrimestre, notaTercerTrimestre);
+
+ let resultado;
+
+if(notaPromedio >=7){
+    resultado = "Aprobado"
+}else{
+    resultado = "Desaprobado"
+}
+
+console.log(`El alumno ${alumno} tiene un promedio de ${notaPromedio} y está ${resultado}.`);
+
+
