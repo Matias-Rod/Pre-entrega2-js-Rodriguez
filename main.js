ab198d6d5605ceced94429a544bb2084856535a8
Matias Rodriguez <matiezequielr@gmail.com>
2023-08-29 19:18:47 -0300

    Se agrego el uso de objetos, arrays, metodo de busca dentro del array

--- main.js
+++ main.js
@@ -1,23 +1,51 @@
-const alumno = prompt("Ingrese el nombre del alumno") ;
-const notaPrimerTrimestre = parseFloat(prompt("Ingrese nota del primer trimestre")) ;
-const notaSegundoTrimestre = parseFloat(prompt("Ingrese nota del segundo trimestre")) ;
-const notaTercerTrimestre = parseFloat(prompt("Ingrese nota del tercer trimestre")) ;
-
-
+// Función para calcular el promedio
 function calcularPromedio(nota1, nota2, nota3) {
     return (nota1 + nota2 + nota3) / 3;
 }
 
-const notaPromedio =calcularPromedio(notaPrimerTrimestre, notaSegundoTrimestre, notaTercerTrimestre);
-
- let resultado;
-
-if(notaPromedio >=7){
-    resultado = "Aprobado"
-}else{
-    resultado = "Desaprobado"
+// Función para crear un objeto de alumno y agregarlo al array
+function agregarAlumno(nombre, nota1, nota2, nota3) {
+    const alumno = {
+        nombre: nombre,
+        notas: [nota1, nota2, nota3],
+        promedio: calcularPromedio(nota1, nota2, nota3)
+    };
+    alumnos.push(alumno);
 }
 
-document.write(`El alumno ${alumno} tiene un promedio de ${notaPromedio.toFixed(2)} y está ${resultado}.`);
+// Función para buscar un alumno por nombre
+function buscarAlumnoPorNombre(nombre) {
+    return alumnos.find(alumno => alumno.nombre === nombre);
+}
 
+const alumnos = []; // Array para almacenar objetos de alumnos
 
+do {
+    const alumnoNombre = prompt("Ingrese el nombre del alumno");
+    const notaPrimerTrimestre = parseFloat(prompt("Ingrese nota del primer trimestre"));
+    const notaSegundoTrimestre = parseFloat(prompt("Ingrese nota del segundo trimestre"));
+    const notaTercerTrimestre = parseFloat(prompt("Ingrese nota del tercer trimestre"));
+
+    agregarAlumno(alumnoNombre, notaPrimerTrimestre, notaSegundoTrimestre, notaTercerTrimestre);
+
+    const alumnoBuscado = buscarAlumnoPorNombre(alumnoNombre);
+    if (alumnoBuscado) {
+        let resultado;
+        if (alumnoBuscado.promedio >= 7) {
+            resultado = "Aprobado";
+        } else {
+            resultado = "Desaprobado";
+        }
+        alert(`El alumno ${alumnoBuscado.nombre} tiene un promedio de ${alumnoBuscado.promedio.toFixed(2)} y está ${resultado}.`);
+    } else {
+        alert("Alumno no encontrado.");
+    }
+
+    const continuar = confirm("¿Desea agregar otro alumno?");
+    if (!continuar) {
+        break; // Salir del bucle si el usuario no desea continuar
+    }
+} while (true); // El bucle se ejecutará siempre que el usuario quiera continuar
+
+// Cierre del programa
+alert("Sesión finalizada!");
